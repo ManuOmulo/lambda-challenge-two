6af553f13e76ca6357d2d7b282e945f0bc634aec
ManuOmulo <omuloemmanuel@gmail.com>
2024-02-25 18:52:18 +0300

    Added a lastname that is a reverse of firstname

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,21 @@ public class Main {
     Arrays.setAll(firstNames, (element) -> firstNames[element].toUpperCase());
     System.out.println(Arrays.toString(firstNames));
 
-//  Added a random middle name initial to the names with a period
+//  Adding a random middle name initial to the names with a period
     Arrays.setAll(firstNames, (element) -> {
       String initial = "%c".formatted(random.nextInt(65, 91));
       return "%s %s.".formatted(firstNames[element], initial);
     });
     System.out.println(Arrays.toString(firstNames));
 
-
+//  Adding a last name that is reverse of the first name
+    Arrays.setAll(firstNames, (element) -> {
+      StringBuilder reversedString = new StringBuilder();
+      int i = firstNames[element].indexOf(" ");
+      String lastName = firstNames[element].substring(0, i);
+      reversedString.append(lastName);
+      return "%s %s".formatted(firstNames[element], reversedString.reverse());
+    });
+    Arrays.asList(firstNames).forEach(element -> System.out.println(element));
   }
 }
